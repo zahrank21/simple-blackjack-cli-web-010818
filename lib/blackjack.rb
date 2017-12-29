65cd965cd6f344606c6e94520e9bae2b3d1c31c4
zahrank21 <zahrank@gmail.com>
2017-12-29 14:41:40 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,7 +37,7 @@ if get_user_input == "s"
   return result.reduce(:+)
 elsif get_user_input == "h"
   result << deal_card
-  result.reduce(:+)
+  puts result.reduce(:+)
 else
   return invalid_command
 end
