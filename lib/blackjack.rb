6172ff5e0a3900d1f473cae91498a38a5abfee6c
zahrank21 <zahrank@gmail.com>
2017-12-29 14:12:59 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -13,19 +13,19 @@ def display_card_total(num)
 end
 
 def prompt_user
-
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
 def end_game
-  # code #end_game here
+  puts "Sorry"
 end
 
 def initial_round
-  # code #initial_round here
+  
 end
 
 def hit?
